5ea909962b42cc3f3466f3f5d199da46478fb4ac
ouo_yoonk <joyk9251@gmail.com>
2022-07-02 22:32:40 +0900

    [update] 커뮤니티 페이지 작업

--- src/page/CommunityPage.tsx
+++ src/page/CommunityPage.tsx
@@ -3,22 +3,23 @@ import styled from 'styled-components';
 import { Select, SelectOption, TextInput, Wrapper } from '../component/element';
 import { NavLayout } from '../component/layout/NavLayout';
 import { PageLayout } from '../component/layout/PageLayout';
-import { CommunitySearchControl } from '../Types/community';
+import { PostCard } from '../component/PostCard';
+import { CommunitySearchControl, Post } from '../Types/community';
 
 const ContentWrapper = styled.div`
-  padding: 1rem;
   height: 100%;
   section:nth-of-type(1) {
     height: 10rem;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    border-bottom: 8px solid ${({ theme }) => theme.color.grayLight};
+    padding: 1rem;
   }
 
   section:nth-of-type(2) {
     overflow-y: scroll;
     height: 100%;
+    background-color: ${({ theme }) => theme.color.grayLight};
   }
 `;
 
@@ -29,12 +30,74 @@ const categoryOptions: SelectOption[] = [
   { value: '232', label: '라' },
 ];
 
+const postList: Post[] = [
+  {
+    id: 1,
+    userId: 1,
+    userImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png',
+    userName: '강남스타일1234',
+    imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png',
+    title: '즐거운 토요일 일 ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷㄷ',
+    content:
+      '어떤 운동이든 상관없이 딱 한시간 운동하기입니다! 어떤 운동이든 상관없이 딱 한시간 운동하기입니다! 어떤 운동이든 상관없이 딱 한시간 운동하기입니다!',
+    type: 'challange',
+    gather: 0,
+  },
+  {
+    id: 2,
+    userId: 2,
+    userImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png',
+    userName: 'gisele',
+    title: '즐거운 토요일',
+    content:
+      '오늘은 토요일 너무 덥다. 너무 더워. 산책언제가지. 이 날씨면 개도 사람도 더위먹지이이 시원해지면 가야겠다. 점심은 뭐먹으면 좋을까. 생각보다 진도가 안나가는군',
+    type: 'challange',
+    gather: 0,
+  },
+  {
+    id: 3,
+    userId: 3,
+    userName: 'henrry',
+    title: '즐거운 토요',
+    content:
+      '오늘은 토요일 너무 덥다. 너무 더워. 산책언제가지. 이 날씨면 개도 사람도 더위먹지이이 시원해지면 가야겠다. 점심은 뭐먹으면 좋을까. 생각보다 진도가 안나가는군',
+    type: 'daily',
+    gather: 2,
+  },
+  {
+    id: 4,
+    userId: 4,
+    userImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png',
+    userName: 'sean',
+    title: '즐거운 토요일',
+    content:
+      '오늘은 토요일 너무 덥다. 너무 더워. 산책언제가지. 이 날씨면 개도 사람도 더위먹지이이 시원해지면 가야겠다. 점심은 뭐먹으면 좋을까. 생각보다 진도가 안나가는군',
+    type: 'challange',
+    gather: 0,
+  },
+  {
+    id: 5,
+    userId: 5,
+    userImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png',
+    userName: 'sean',
+    imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png',
+    title: '즐거운 토요일',
+    content:
+      '오늘은 토요일 너무 덥다. 너무 더워. 산책언제가지. 이 날씨면 개도 사람도 더위먹지이이 시원해지면 가야겠다. 점심은 뭐먹으면 좋을까. 생각보다 진도가 안나가는군',
+    type: 'challange',
+    gather: 2,
+  },
+];
 export const CommunityPage = () => {
   const [control, setControl] = useState<CommunitySearchControl>({
     category: 'title',
     keyword: '',
     type: 'all',
   });
+
+  const onClickPost = (id: string) => {
+    console.log('포스트클릭', id);
+  };
 
   return (
     <NavLayout>
@@ -67,7 +130,11 @@ export const CommunityPage = () => {
               onChange={(value) => setControl({ ...control, category: value })}
             />
           </section>
-          <section>게시글들!!</section>
+          <section>
+            {postList.map((post) => (
+              <PostCard key={post.id} {...post} onClick={onClickPost} />
+            ))}
+          </section>
         </ContentWrapper>
       </PageLayout>
     </NavLayout>
