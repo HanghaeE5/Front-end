7239f5279bb0646381378dfe84f3c595a6d213cd
ouo_yoonk <joyk9251@gmail.com>
2022-07-07 21:02:12 +0900

    [create] TextArea 추가, input custom hook 추가

--- src/component/element/TextInput.tsx
+++ src/component/element/TextInput.tsx
@@ -3,6 +3,7 @@ import { BiSearch } from 'react-icons/bi';
 import { Wrapper } from './Wrapper';
 
 interface TextInputProps {
+  type?: 'text' | 'area';
   onChange: (value: string) => void;
   onSearch?: (value: string) => void;
   placeholder?: string;
@@ -35,18 +36,45 @@ const SearchButton = styled(BiSearch)`
   cursor: pointer;
 `;
 
-export const TextInput = ({ placeholder, showSearch, value, onChange }: TextInputProps) => {
+const TextAreaElement = styled.textarea`
+  height: 11.25rem;
+  border-radius: ${(props) => props.theme.radius};
+  background-color: ${(props) => props.theme.color.grayLight};
+  border: none;
+  padding: ${(props) => props.theme.inputPadding};
+  ::placeholder {
+    color: ${(props) => props.theme.color.grayMedium};
+    font-size: 0.813rem;
+  }
+  width: 100%;
+  font-family: 'NotoRegu';
+  font-weight: 400;
+  resize: none;
+`;
+
+export const TextInput = ({ type = 'text', placeholder, showSearch, value, onChange }: TextInputProps) => {
+  if (type === 'text') {
+    return (
+      <Wrapper>
+        <TextElement
+          placeholder={placeholder}
+          value={value}
+          onChange={(e) => {
+            onChange(e.target.value);
+          }}
+        />
+        {showSearch && <SearchButton onClick={() => showSearch.onSearch()} />}
+      </Wrapper>
+    );
+  }
+
   return (
-    <Wrapper>
-      <TextElement
-        type="text"
-        placeholder={placeholder}
-        value={value}
-        onChange={(e) => {
-          onChange(e.target.value);
-        }}
-      />
-      {showSearch && <SearchButton onClick={() => showSearch.onSearch()} />}
-    </Wrapper>
+    <TextAreaElement
+      placeholder={placeholder}
+      value={value}
+      onChange={(e) => {
+        onChange(e.target.value);
+      }}
+    />
   );
 };
