779d81c0f6e85dd3b66184ac9e01642031801853
ouo_yoonk <joyk9251@gmail.com>
2022-07-07 21:23:33 +0900

    [improve] Button 컴포넌트에 dashed 타입 추가

--- src/component/element/Button.tsx
+++ src/component/element/Button.tsx
@@ -2,11 +2,12 @@ import { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 
 interface ButtonProps {
-  buttonType?: 'primary' | 'default' | 'disable';
+  buttonType?: 'primary' | 'default' | 'disable' | 'dashed';
   size?: 'small' | 'large';
   isSquare?: boolean;
   onClick?: () => void;
   margin?: string;
+  width?: string;
 }
 
 const StyledButton = styled.button<ButtonProps>`
@@ -14,20 +15,26 @@ const StyledButton = styled.button<ButtonProps>`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
+  width: ${(props) => props.width || '100%'};
   font-weight: 700;
   font-family: 'Noto Sans CJK Light KR';
   font-size: ${({ size }) => (size === 'small' ? '0.875rem' : '1rem')};
   padding: 1rem 0;
-  color: ${({ buttonType, theme }) => (buttonType === 'default' ? theme.color.grayMediumDark : 'white')};
-  border: 1px solid;
-  border-color: ${({ buttonType, theme }) => (buttonType === 'default' ? theme.color.grayMediumDark : '')};
-  border-radius: ${({ isSquare, theme }) => (isSquare ? '' : theme.radius)};
-  background-color: ${({ buttonType, theme }) =>
-    buttonType === 'default' ? 'white' : buttonType === 'primary' ? theme.color.grayDark : theme.color.grayMediumLight};
   height: ${({ size }) => (size === 'small' ? '3rem' : '4rem')};
   cursor: ${({ buttonType }) => buttonType && 'disable'};
   margin: ${({ margin }) => margin && margin};
+
+  color: ${({ buttonType, theme }) =>
+    buttonType === 'default' ? theme.color.grayMediumDark : buttonType === 'dashed' ? theme.color.grayDark : 'white'};
+  border: 1px ${({ buttonType }) => (buttonType === 'dashed' ? 'dashed' : 'solid')};
+  border-color: ${({ buttonType, theme }) => (buttonType === 'default' ? theme.color.grayMediumDark : '')};
+  border-radius: ${({ isSquare, theme }) => (isSquare ? '' : theme.radius)};
+  background-color: ${({ buttonType, theme }) =>
+    buttonType === 'default' || buttonType === 'dashed'
+      ? 'white'
+      : buttonType === 'primary'
+      ? theme.color.grayDark
+      : theme.color.grayMediumLight};
 `;
 
 export const Button = ({
