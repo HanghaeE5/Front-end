87ae91a1ef855a9587635122e38652c15aa8d1c7
ouo_yoonk <joyk9251@gmail.com>
2022-07-13 14:24:26 +0900

    [improve] eslint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,17 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
     // 'simple-import-sort/imports': 'error',
     // 'simple-import-sort/exports': 'error',
   },
   parser: '@typescript-eslint/parser',
-  //   parserOptions: {
-  //     ecmaFeatures: {
-  //       jsx: true,
-  //     },
-  //     ecmaVersion: 'latest',
-  //     sourceType: 'module',
-  //   },
-  //   plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
 };
