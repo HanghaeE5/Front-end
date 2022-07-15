14887fcd42d3ace814f975722d2817d58e5a0e90
ccimayoung <ccimayoung@gmail.com>
2022-07-16 06:20:10 +0900

    긴급 배포로 eslint 잠시 off

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    // 'simple-import-sort/imports': 'error',
-    // 'simple-import-sort/exports': 'error',
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
-};
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//     node: true,
+//   },
+//   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+//   rules: {
+//     'react/react-in-jsx-scope': 'off',
+//     'no-unused-vars': 'off',
+//     '@typescript-eslint/no-unused-vars': 'off',
+//     // 'simple-import-sort/imports': 'error',
+//     // 'simple-import-sort/exports': 'error',
+//   },
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//     ecmaVersion: 'latest',
+//     sourceType: 'module',
+//   },
+//   plugins: ['react', '@typescript-eslint', 'simple-import-sort'],
+// };
