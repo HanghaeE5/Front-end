08a87a17e688ab9c314d1cf2bd4c72c6f49ab5f0
ouo_yoonk <joyk9251@gmail.com>
2022-07-20 11:44:00 +0900

    [improve] font 관련 로직 수정

--- src/component/element/Typography.tsx
+++ src/component/element/Typography.tsx
@@ -4,10 +4,10 @@ import styled from 'styled-components';
 const SteyledTypo = styled.span<TypographyProps>`
   font-size: ${({ size }) => `${size}rem`};
   color: ${({ color }) => color};
-  font-weight: ${({ weight }) => weight};
   cursor: ${({ isPointer }) => (isPointer ? 'pointer' : '')};
   text-decoration: ${({ underline }) => underline && 'underline'};
-  font-family: ${({ isBold }) => (isBold ? 'NotoBold' : 'NotoLight')};
+  font-family: 'NotoSans';
+  font-weight: ${({ weight }) => weight || 500};
   white-space: pre-line;
   text-align: ${({ align }) => align || 'left'};
   line-height: ${({ lineHeight }) => `${lineHeight}rem`}; ;
