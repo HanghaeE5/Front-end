1baed7474e48bac8d80186f7c1f7e95d8084e212
ouo_yoonk <joyk9251@gmail.com>
2022-07-22 15:37:09 +0900

    [improve] 커뮤니티 글 공통 컨펌 적용

--- src/page/CommunityDetailPage.tsx
+++ src/page/CommunityDetailPage.tsx
@@ -3,7 +3,6 @@ import { NavLayout } from '../component/layout/NavLayout';
 import { PageLayout } from '../component/layout/PageLayout';
 import { PostCard } from '../component/PostCard';
 import { Board } from '../Types/community';
-import { usePopConfirm } from '../hooks/usePopConfirm';
 import { useNavigate, useParams } from 'react-router';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import {
@@ -16,11 +15,10 @@ import {
 import { PATH } from '../route/routeList';
 import { useRecoilValue } from 'recoil';
 import { userInfoState } from '../recoil/store';
-import { ReactComponent as WithTodo } from '../asset/icons/icon_withtodo.svg';
-import { ReactComponent as Chat } from '../asset/icons/icon_chat.svg';
 import { chattingApi } from '../api/callApi';
 import { AxiosError } from 'axios';
 import { useState } from 'react';
+import { useCommonConfirm } from '../hooks/useCommonConfirm';
 
 export const CommunityDetailPage = () => {
   const nav = useNavigate();
@@ -28,7 +26,6 @@ export const CommunityDetailPage = () => {
   const userInfo = useRecoilValue(userInfoState);
   const [publicChattingRoomId, SetpublicChattingRoomId] = useState<string>('');
 
-  // TODO : 얘도 훅으로 빼기
   const queryClient = useQueryClient();
 
   const refectchBoardList = () => {
@@ -47,15 +44,12 @@ export const CommunityDetailPage = () => {
     },
   });
 
-  const { visible: visibleChallange, openConfirm: openChallange, closeConfirm: closeChallange } = usePopConfirm();
-  const { visible: visibleChat, openConfirm: openChatConfirm, closeConfirm: closeChatConfirm } = usePopConfirm();
-  const { visible: visibleError, openConfirm: openErrorConfirm, closeConfirm: closeErrorConfirm } = usePopConfirm();
-  const { visible: visibleCancel, openConfirm: openCancelConfirm, closeConfirm: closeCacnelConfirm } = usePopConfirm();
-  const {
-    visible: copiedConfirmVisible,
-    openConfirm: openCopiedConfirm,
-    closeConfirm: closeCopiedConfirm,
-  } = usePopConfirm();
+  const { openSuccessConfirm, openErrorConfirm } = useCommonConfirm();
+
+  // const { visible: visibleChallange, openConfirm: openChallange, closeConfirm: closeChallange } = usePopConfirm();
+  //const { visible: visibleChat, openConfirm: openChatConfirm, closeConfirm: closeChatConfirm } = usePopConfirm();
+  // const { visible: visibleError, openConfirm: openError2Confirm, closeConfirm: closeErrorConfirm } = usePopConfirm();
+  // const { visible: visibleCancel, openConfirm: openCancelConfirm, closeConfirm: closeCacnelConfirm } = usePopConfirm();
 
   const {
     data: postDetail,
@@ -70,27 +64,9 @@ export const CommunityDetailPage = () => {
   const { mutate: joinChallenge } = useMutation(joinChallengeFn);
   const { mutate: cancelChallenge } = useMutation(cancelChallengeFn);
   const { mutate: deleteBoard } = useMutation(deleteBoardFn, {
-    onSuccess: () => {
-      setConfirmState((prev) => ({
-        iconType: 'success',
-        visible: true,
-        title: '게시글을 삭제했습니다.',
-        button: {
-          text: '닫기',
-          onClick: () => {
-            setConfirmState((prev) => ({ ...prev, visible: false }));
-            refectchBoardList();
-          },
-        },
-      }));
-    },
-    onError: () =>
-      setConfirmState((prev) => ({
-        ...prev,
-        visible: true,
-        title: '삭제에 실패했습니다.',
-        button: { text: '닫기', onClick: () => setConfirmState((prev) => ({ ...prev, visible: false })) },
-      })),
+    onSuccess: () =>
+      openSuccessConfirm({ title: '게시글을 삭제했습니다.', button: { text: '확인', onClick: refectchBoardList } }),
+    onError: () => openErrorConfirm({}),
   });
 
   const onConfirmChallenge = () => {
@@ -98,12 +74,32 @@ export const CommunityDetailPage = () => {
 
     joinChallenge(postDetail?.boardId, {
       onSuccess: () => {
-        closeChallange();
-        openChatConfirm();
+        setConfirmState({
+          visible: true,
+          iconType: 'chat',
+          title: '채팅방에도 참여하시겠어요?',
+          button: {
+            text: '네',
+            onClick: () => {
+              refetchBoardDetail();
+              SetpublicChattingRoomId(postDetail.chatRoomId);
+              enterPublicChattingRoom({ roomId: postDetail.chatRoomId });
+              setConfirmState((prev) => ({ ...prev, visible: false }));
+            },
+          },
+          optionalButton: {
+            text: ' 아니오',
+            onClick: () => {
+              refetchBoardDetail();
+              setConfirmState((prev) => ({ ...prev, visible: false }));
+            },
+          },
+        });
       },
+
       onError: () => {
-        alert('처리에 실패했습니다');
-        closeChallange();
+        setConfirmState((prev) => ({ ...prev, visible: false }));
+        openErrorConfirm({});
       },
     });
   };
@@ -112,7 +108,7 @@ export const CommunityDetailPage = () => {
     if (!postDetail) return;
 
     if (postDetail.category === 'CHALLENGE') {
-      openErrorConfirm();
+      openErrorConfirm({ title: `위드 투 두 게시물은 작성 이후  \n 수정, 삭제가 불가합니다` });
       return;
     }
     nav(`${PATH.COMMUNITY_POST}/${postDetail.boardId}`);
@@ -122,7 +118,7 @@ export const CommunityDetailPage = () => {
     if (!postDetail) return;
 
     if (postDetail.category === 'CHALLENGE') {
-      openErrorConfirm();
+      openErrorConfirm({ title: `위드 투 두 게시물은 작성 이후  \n 수정, 삭제가 불가합니다` });
       return;
     }
     deleteBoard(postDetail.boardId);
@@ -132,17 +128,48 @@ export const CommunityDetailPage = () => {
     const url = window.location.protocol + '//' + window.location.host + '/' + window.location.pathname;
     navigator.clipboard.writeText(url);
 
-    openCopiedConfirm();
+    openSuccessConfirm({ title: '게시글 주소를 복사했습니다.' });
   };
 
-  const onClickButton = () => {
+  const onClickParticipateButton = () => {
     if (postDetail?.withTodoDeadline) return;
 
     if (postDetail?.participating) {
-      // 참여취소
-      openCancelConfirm();
+      setConfirmState({
+        visible: true,
+        iconType: 'warning',
+        title: `위드 투 두 참여를 \n 취소하시겠어요?`,
+        content: '모집 마감일 이후 취소가 불가합니다',
+        button: {
+          text: '네',
+          onClick: () => {
+            cancelChallenge(postDetail.boardId, {
+              onSuccess: () => {
+                refetchBoardDetail();
+                setConfirmState((prev) => ({ ...prev, visible: false }));
+              },
+            });
+
+            exitChattingRoom({ roomId: postDetail.chatRoomId });
+          },
+        },
+        optionalButton: {
+          text: ' 아니오',
+          onClick: () => setConfirmState((prev) => ({ ...prev, visible: false })),
+        },
+      });
     } else {
-      openChallange();
+      setConfirmState({
+        visible: true,
+        iconType: 'withTodo',
+        title: '위드 투 두에 참여하시겠어요?',
+        content: '모집 마감일 이후 취소가 불가합니다',
+        button: { text: '네', onClick: onConfirmChallenge },
+        optionalButton: {
+          text: ' 아니오',
+          onClick: () => setConfirmState((prev) => ({ ...prev, visible: false })),
+        },
+      });
     }
   };
 
@@ -172,85 +199,6 @@ export const CommunityDetailPage = () => {
   return (
     <NavLayout>
       {confirmState.visible && <PopConfirmNew {...confirmState} />}
-      {visibleError && (
-        <PopConfirmNew
-          iconType="warning"
-          title={`위드 투 두 게시물은 작성 이후  \n 수정, 삭제가 불가합니다`}
-          button={{ text: '확인', onClick: closeErrorConfirm }}
-        />
-      )}
-
-      {visibleChallange && (
-        <PopConfirmNew
-          iconType="withTodo"
-          title="위드 투 두에 참여하시겠어요?"
-          content="모집 마감일 이후 취소가 불가합니다"
-          button={{ text: '네', onClick: onConfirmChallenge }}
-          optionalButton={{
-            text: ' 아니오',
-            onClick: () => {
-              closeChallange();
-            },
-          }}
-        />
-      )}
-      {visibleChat && (
-        <PopConfirmNew
-          iconType="chat"
-          title="채팅방에도 참여하시겠어요?"
-          button={{
-            text: '네',
-            onClick: () => {
-              // TODO: 채팅방 참여하기 로직
-              refetchBoardDetail();
-              closeChatConfirm();
-              SetpublicChattingRoomId(postDetail.chatRoomId);
-              enterPublicChattingRoom({ roomId: postDetail.chatRoomId });
-            },
-          }}
-          optionalButton={{
-            text: ' 아니오',
-            onClick: () => {
-              refetchBoardDetail();
-              closeChatConfirm();
-            },
-          }}
-        />
-      )}
-      {visibleCancel && (
-        <PopConfirmNew
-          iconType="warning"
-          title={'위드 투 두 참여를 \n 취소하시겠어요?'}
-          button={{
-            text: '네',
-            onClick: () => {
-              cancelChallenge(postDetail.boardId, {
-                onSuccess: () => {
-                  refetchBoardDetail();
-                  closeCacnelConfirm();
-                },
-              });
-
-              exitChattingRoom({ roomId: postDetail.chatRoomId });
-            },
-          }}
-          optionalButton={{
-            text: ' 아니오',
-            onClick: () => closeCacnelConfirm(),
-          }}
-        />
-      )}
-      {copiedConfirmVisible && (
-        <PopConfirmNew
-          iconType="success"
-          title={'게시글 주소를 복사했습니다'}
-          button={{
-            text: '확인',
-            onClick: () => closeCopiedConfirm(),
-          }}
-        />
-      )}
-
       <PageLayout title="커뮤니티">
         <Wrapper isColumn alignItems="start" height="100%" justifyContent="space-between">
           <Wrapper isColumn alignItems="start" height="80%">
@@ -280,7 +228,7 @@ export const CommunityDetailPage = () => {
               <Button
                 margin="1rem"
                 buttonType={postDetail.withTodoDeadline ? 'disable' : 'primary'}
-                onClick={onClickButton}
+                onClick={onClickParticipateButton}
               >
                 {postDetail.withTodoDeadline
                   ? '마감되었습니다'
