f787ce681821d133aef1cfbe5e7e5b0543e30ed0
ccimayoung <ccimayoung@gmail.com>
2022-08-03 08:45:52 +0900

    [update]알림 수정중

--- src/component/layout/TopNavBar.tsx
+++ src/component/layout/TopNavBar.tsx
@@ -1,5 +1,5 @@
 import { AxiosError } from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilState, useRecoilValue } from 'recoil';
@@ -111,6 +111,57 @@ export const TopNavBar = ({ isWithBanner }: { isWithBanner?: boolean }) => {
     }
   }, [userInformData]);
 
+  const [alarmStore, setAlarmStore] = useState<any>([]);
+  function alarm() {
+    const id = userInfoData?.id;
+
+    const eventSource = new EventSource(`https://todowith.shop/subscribe/${id}`);
+
+    eventSource.addEventListener('sse', function (event) {
+      console.log(event.data);
+      setAlarmStore([...alarmStore, event.data]);
+
+      const data = JSON.parse(event.data);
+
+      (async () => {
+        // 브라우저 알림
+        const showNotification = () => {
+          const notification = new Notification('코드 봐줘', {
+            body: data.content,
+          });
+
+          setTimeout(() => {
+            notification.close();
+          }, 10 * 1000);
+
+          notification.addEventListener('click', () => {
+            window.open(data.url, '_blank');
+          });
+        };
+
+        // 브라우저 알림 허용 권한
+        let granted = false;
+
+        if (Notification.permission === 'granted') {
+          granted = true;
+        } else if (Notification.permission !== 'denied') {
+          const permission = await Notification.requestPermission();
+          granted = permission === 'granted';
+        }
+
+        // 알림 보여주기
+        if (granted) {
+          showNotification();
+        }
+      })();
+    });
+  }
+  // useEffect(() => {
+  //   if (userInformData.status === 'success') {
+  //     alarm();
+  //   }
+  // }, [alarmStore]);
+  // console.log(alarmStore);
   return (
     <TopNavWrapper>
       {/* {confirmState.visible && <PopConfirmNew {...confirmState} />} */}
